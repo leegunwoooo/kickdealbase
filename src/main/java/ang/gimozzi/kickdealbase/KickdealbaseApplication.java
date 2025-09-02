7ad93d54f2d0aad28db2ae5205a1195f298e8971
leegunwoo <leegunwoo0325@gmail.com>
2025-09-02 19:24:31 +0900

    난 커밋컨베션 따위 지키지 않아

---
+++ src/main/java/ang/gimozzi/kickdealbase/KickdealbaseApplication.java
@@ -0,0 +1,13 @@
+package ang.gimozzi.kickdealbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickdealbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickdealbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ang/gimozzi/kickdealbase/KickdealbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.gimozzi.kickdealbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickdealbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
