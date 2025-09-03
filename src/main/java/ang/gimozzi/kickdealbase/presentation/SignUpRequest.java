688a8ea2f6fe4906c1f82c03e45e172bb4a4f4d5
leegunwoo <leegunwoo0325@gmail.com>
2025-09-03 10:37:47 +0900

    feat(#1) : 회원가입

---
+++ src/main/java/ang/gimozzi/kickdealbase/presentation/SignUpRequest.java
@@ -0,0 +1,18 @@
+package ang.gimozzi.kickdealbase.presentation;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class SignUpRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+}
