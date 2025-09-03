df2e7eee61196aa64d74fe423ce467d3d5da6c9d
leegunwoo <leegunwoo0325@gmail.com>
2025-09-03 15:34:07 +0900

    리프레쉬

---
+++ src/main/java/ang/gimozzi/kickdealbase/application/user/RefreshUseCase.java
@@ -0,0 +1,21 @@
+package ang.gimozzi.kickdealbase.application.user;
+
+import ang.gimozzi.kickdealbase.infrastructure.jwt.dto.response.TokenResponse;
+import ang.gimozzi.kickdealbase.infrastructure.jwt.service.TokenService;
+import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class RefreshUseCase {
+
+    private final TokenService tokenService;
+
+    public TokenResponse execute(String refreshToken) {
+        return new TokenResponse(
+                tokenService.refreshAccessToken(refreshToken),
+                refreshToken
+        );
+    }
+
+}
