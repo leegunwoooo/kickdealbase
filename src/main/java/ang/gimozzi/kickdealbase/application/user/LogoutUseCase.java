cda76396dea53c4a9a540f5d2ced68b96bd41415
leegunwoo <leegunwoo0325@gmail.com>
2025-09-03 15:33:54 +0900

    로그아웃

---
+++ src/main/java/ang/gimozzi/kickdealbase/application/user/LogoutUseCase.java
@@ -0,0 +1,19 @@
+package ang.gimozzi.kickdealbase.application.user;
+
+import ang.gimozzi.kickdealbase.domain.user.User;
+import ang.gimozzi.kickdealbase.infrastructure.UserRepository;
+import ang.gimozzi.kickdealbase.infrastructure.jwt.domain.RefreshTokenRepository;
+import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class LogoutUseCase {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public void execute(User user) {
+        refreshTokenRepository.deleteById(user.getId());
+    }
+
+}
