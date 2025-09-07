a1b815ea979906b404fa49780f94e7eee0601837
leegunwoo <leegunwoo0325@gmail.com>
2025-09-08 00:33:33 +0900

    상품 엔티티 클래스 작성

---
+++ src/main/java/ang/gimozzi/kickdealbase/domain/product/Product.java
@@ -0,0 +1,27 @@
+package ang.gimozzi.kickdealbase.domain.product;
+
+import ang.gimozzi.kickdealbase.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    private User seller;
+
+}
