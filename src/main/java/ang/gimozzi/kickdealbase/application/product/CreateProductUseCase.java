33b831b90554d78e85877225db58d888eeb44c0f
leegunwoo <leegunwoo0325@gmail.com>
2025-09-11 08:22:58 +0900

    서비스 작성

---
+++ src/main/java/ang/gimozzi/kickdealbase/application/product/CreateProductUseCase.java
@@ -0,0 +1,31 @@
+package ang.gimozzi.kickdealbase.application.product;
+
+import ang.gimozzi.kickdealbase.domain.product.Product;
+import ang.gimozzi.kickdealbase.domain.user.User;
+import ang.gimozzi.kickdealbase.infrastructure.persistence.ProductRepository;
+import ang.gimozzi.kickdealbase.infrastructure.s3.S3Service;
+import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@UseCase
+@RequiredArgsConstructor
+public class CreateProductUseCase {
+
+    private final ProductRepository productRepository;
+    private final S3Service s3Service;
+
+    public ProductResponse createProduct(ProductRequest request, MultipartFile image, User user){
+        String url = s3Service.uploadFile(image);
+
+        return new ProductResponse(productRepository.save(
+                Product.builder()
+                        .name(request.getName())
+                        .description(request.getDescription())
+                        .seller(user)
+                        .imageUrl(url)
+                        .build()
+        ));
+    }
+
+}
