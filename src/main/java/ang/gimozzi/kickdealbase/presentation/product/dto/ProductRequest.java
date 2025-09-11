057340549361f054a2c15fe3b168ec67d4509885
leegunwoo <leegunwoo0325@gmail.com>
2025-09-11 11:19:26 +0900

    리퀘스트 작성

---
+++ src/main/java/ang/gimozzi/kickdealbase/presentation/product/dto/ProductRequest.java
@@ -0,0 +1,12 @@
+package ang.gimozzi.kickdealbase.presentation.product.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProductRequest {
+
+    private String name;
+
+    private String description;
+
+}
