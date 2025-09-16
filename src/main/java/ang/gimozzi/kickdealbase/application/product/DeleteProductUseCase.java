d2b24d05a45b6f7bcd07bc43a98d761505a02429
leegunwoo <leegunwoo0325@gmail.com>
2025-09-16 10:53:59 +0900

    삭제 서비스

---
+++ src/main/java/ang/gimozzi/kickdealbase/application/product/DeleteProductUseCase.java
@@ -0,0 +1,33 @@
+package ang.gimozzi.kickdealbase.application.product;
+
+import ang.gimozzi.kickdealbase.domain.product.Product;
+import ang.gimozzi.kickdealbase.domain.product.service.ProductFacade;
+import ang.gimozzi.kickdealbase.domain.user.User;
+import ang.gimozzi.kickdealbase.infrastructure.persistence.ProductRepository;
+import ang.gimozzi.kickdealbase.infrastructure.s3.S3Service;
+import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class DeleteProductUseCase {
+
+    private final ProductFacade productFacade;
+    private final ProductRepository productRepository;
+    private final S3Service s3Service;
+
+    public String deleteProduct(Long productId, User user){
+
+        Product product = productFacade.getProduct(productId);
+
+        product.validUser(user);
+
+        s3Service.deleteFile(product.getImageUrl());
+
+        productRepository.delete(product);
+
+        return "삭제되었습니다.";
+
+    }
+
+}
