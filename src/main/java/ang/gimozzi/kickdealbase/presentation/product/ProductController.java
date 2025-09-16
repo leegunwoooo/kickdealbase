729fba35d853c4fc3ca66d6f95b3e417d359048d
leegunwoo <leegunwoo0325@gmail.com>
2025-09-16 11:00:21 +0900

    컨트롤러 작성

--- src/main/java/ang/gimozzi/kickdealbase/presentation/product/ProductController.java
+++ src/main/java/ang/gimozzi/kickdealbase/presentation/product/ProductController.java
@@ -1,6 +1,6 @@
 package ang.gimozzi.kickdealbase.presentation.product;
 
-import ang.gimozzi.kickdealbase.application.product.CreateProductUseCase;
+import ang.gimozzi.kickdealbase.application.product.*;
 import ang.gimozzi.kickdealbase.domain.user.User;
 import ang.gimozzi.kickdealbase.presentation.product.dto.ProductRequest;
 import ang.gimozzi.kickdealbase.presentation.product.dto.ProductResponse;
@@ -8,16 +8,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RequiredArgsConstructor
-@RestController("/product")
+@RestController("/products")
 public class ProductController {
 
     private final CreateProductUseCase createProductUseCase;
+    private final UpdateProductUseCase updateProductUseCase;
+    private final ViewAllProductUseCase viewAllProductUseCase;
+    private final ViewOneProductUseCase viewOneProductUseCase;
+    private final DeleteProductUseCase deleteProductUseCase;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ProductResponse> createProduct(
@@ -26,6 +30,36 @@ public class ProductController {
             @AuthenticationPrincipal User user
     ) {
         return ResponseEntity.ok(createProductUseCase.createProduct(productRequest, image, user));
+    }
+
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, value = "/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @RequestPart("product") ProductRequest productRequest,
+            @RequestPart("image") MultipartFile image,
+            @AuthenticationPrincipal User user,
+            @PathVariable Long id
+    ){
+        return ResponseEntity.ok(updateProductUseCase.updateProduct(productRequest, image, user, id));
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return viewAllProductUseCase.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(viewOneProductUseCase.getOneProduct(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteProduct(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user
+    ) {
+        return ResponseEntity.ok(deleteProductUseCase.deleteProduct(id, user));
     }
 
 }
