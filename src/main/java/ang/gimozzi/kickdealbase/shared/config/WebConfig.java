c536024638aee22286d2fb239a6b6189e4f5a5da
leegunwoo <leegunwoo0325@gmail.com>
2025-09-29 12:10:18 +0900

    cors 설정 추가

---
+++ src/main/java/ang/gimozzi/kickdealbase/shared/config/WebConfig.java
@@ -0,0 +1,19 @@
+package ang.gimozzi.kickdealbase.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:300*", "http://localhost:517*", "http://127.0.0.1:300*", "http://127.0.0.1:517*", "https://horizon-studio-eight.vercel.app")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
