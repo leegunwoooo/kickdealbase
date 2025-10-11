629db02721fc372014bce23edea3bbedaba54290
leegunwoo <leegunwoo0325@gmail.com>
2025-10-12 02:39:41 +0900

    응답 객체 작성

---
+++ src/main/java/ang/gimozzi/kickdealbase/presentation/pay/dto/PaymentResponse.java
@@ -0,0 +1,12 @@
+package ang.gimozzi.kickdealbase.presentation.pay.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PaymentResponse {
+
+    private String orderName;
+
+    private int totalAmount;
+
+}
