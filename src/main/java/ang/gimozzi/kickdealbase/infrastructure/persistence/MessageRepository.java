ae7fa3507ba50ef3533c1fd7b464d129688c5de0
leegunwoo <leegunwoo0325@gmail.com>
2025-12-01 23:07:01 +0900

    i want work

--- src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/MessageRepository.java
+++ src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/MessageRepository.java
@@ -1,9 +1,13 @@
 package ang.gimozzi.kickdealbase.infrastructure.persistence;
 
+import ang.gimozzi.kickdealbase.domain.chatroom.ChatRoom;
 import ang.gimozzi.kickdealbase.domain.message.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByRoom(ChatRoom chatRoom);
 }
