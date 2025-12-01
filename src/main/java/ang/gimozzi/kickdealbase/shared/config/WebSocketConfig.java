63385182958cb7d09696ff30e2da09e02dd647a0
leegunwoo <leegunwoo0325@gmail.com>
2025-12-01 13:52:32 +0900

    i want work

--- src/main/java/ang/gimozzi/kickdealbase/shared/config/WebSocketConfig.java
+++ src/main/java/ang/gimozzi/kickdealbase/shared/config/WebSocketConfig.java
@@ -13,7 +13,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
