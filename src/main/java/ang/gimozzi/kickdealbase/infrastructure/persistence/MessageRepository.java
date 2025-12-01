a75bf07925f3704c41930a151b72bdd9f92993f0
leegunwoo <leegunwoo0325@gmail.com>
2025-12-01 13:18:59 +0900

    i want work

--- src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/MessageRepository.java
+++ src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/MessageRepository.java
@@ -3,8 +3,5 @@ package ang.gimozzi.kickdealbase.infrastructure.persistence;
 import ang.gimozzi.kickdealbase.domain.message.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByRoomIdOrderByTimestampAsc(Long roomId);
 }
