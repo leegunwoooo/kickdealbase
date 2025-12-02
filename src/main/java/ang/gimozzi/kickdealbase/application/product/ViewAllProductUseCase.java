690d53f313647015735cd7829a441a74917bd1d2
leegunwoo <leegunwoo0325@gmail.com>
2025-12-02 11:10:14 +0900

    i want working

--- src/main/java/ang/gimozzi/kickdealbase/application/product/ViewAllProductUseCase.java
+++ src/main/java/ang/gimozzi/kickdealbase/application/product/ViewAllProductUseCase.java
@@ -1,5 +1,6 @@
 package ang.gimozzi.kickdealbase.application.product;
 
+import ang.gimozzi.kickdealbase.domain.product.Product;
 import ang.gimozzi.kickdealbase.infrastructure.persistence.ProductRepository;
 import ang.gimozzi.kickdealbase.infrastructure.s3.S3Service;
 import ang.gimozzi.kickdealbase.presentation.product.dto.ProductResponse;
@@ -7,6 +8,7 @@ import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 
 @UseCase
@@ -20,6 +22,7 @@ public class ViewAllProductUseCase {
     public List<ProductResponse> getAllProducts() {
         return productRepository.findAll()
                 .stream()
+                .sorted(Comparator.comparing(Product::getId).reversed())
                 .map(p -> new ProductResponse(p, s3Service.generateFileUrl(p.getImageUrl())))
                 .toList();
     }
