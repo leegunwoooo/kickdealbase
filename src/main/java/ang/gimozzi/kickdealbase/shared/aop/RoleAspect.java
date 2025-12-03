3857e60bf5e018dd4cea22e4296c8d9337540d73
leegunwoo <leegunwoo0325@gmail.com>
2025-12-03 18:44:56 +0900

    할복

--- src/main/java/ang/gimozzi/kickdealbase/shared/aop/RoleAspect.java
+++ src/main/java/ang/gimozzi/kickdealbase/shared/aop/RoleAspect.java
@@ -2,14 +2,18 @@ package ang.gimozzi.kickdealbase.shared.aop;
 
 import ang.gimozzi.kickdealbase.shared.annotation.Admin;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class RoleAspect {
 
-    @Before("@annotation(Admin)")
+    @Before("@annotation(admin)")
     public void checkAdmin(JoinPoint joinPoint, Admin admin) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth == null || !auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
