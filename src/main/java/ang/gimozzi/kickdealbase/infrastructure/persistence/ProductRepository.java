133725bbccec3b796110d190505138691e31c98d
leegunwoo <leegunwoo0325@gmail.com>
2025-12-05 08:48:34 +0900

    i want it work

--- src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/ProductRepository.java
+++ src/main/java/ang/gimozzi/kickdealbase/infrastructure/persistence/ProductRepository.java
@@ -3,6 +3,8 @@ package ang.gimozzi.kickdealbase.infrastructure.persistence;
 import ang.gimozzi.kickdealbase.domain.product.Category;
 import ang.gimozzi.kickdealbase.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,11 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findProductsByCategory(Category category);
-    List<Product> findProductsByNameContaining(String keyword);
+
+    @Query(
+            value = "SELECT * FROM tbl_product " +
+                    "WHERE name_tsv @@ plainto_tsquery('simple', :keyword)",
+            nativeQuery = true
+    )
+    List<Product> findProductsByNameContaining(@Param("keyword") String keyword);
 }
