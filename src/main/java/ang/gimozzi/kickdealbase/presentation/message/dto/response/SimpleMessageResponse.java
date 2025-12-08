0079aabff88803fc24a05f88841265ee9150efea
leegunwoo <leegunwoo0325@gmail.com>
2025-12-08 19:34:25 +0900

    i want to work

--- src/main/java/ang/gimozzi/kickdealbase/presentation/message/dto/response/SimpleMessageResponse.java
+++ src/main/java/ang/gimozzi/kickdealbase/presentation/message/dto/response/SimpleMessageResponse.java
@@ -3,12 +3,15 @@ package ang.gimozzi.kickdealbase.presentation.message.dto.response;
 import ang.gimozzi.kickdealbase.domain.message.Message;
 import ang.gimozzi.kickdealbase.domain.user.User;
 
+import java.time.LocalDateTime;
+
 public record SimpleMessageResponse(
         Long id,
         String content,
-        User sender
+        User sender,
+        LocalDateTime timeStamp
 ) {
     public static SimpleMessageResponse from(Message message) {
-        return new SimpleMessageResponse(message.getId(), message.getContent(), message.getSender());
+        return new SimpleMessageResponse(message.getId(), message.getContent(), message.getSender(), message.getTimestamp());
     }
 }
