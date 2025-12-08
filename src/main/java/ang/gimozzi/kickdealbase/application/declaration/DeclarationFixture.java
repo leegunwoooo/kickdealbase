1808036226bdc447d766c73e64649596ae9b9ca0
leegunwoo <leegunwoo0325@gmail.com>
2025-12-08 09:04:01 +0900

    i want to work

---
+++ src/main/java/ang/gimozzi/kickdealbase/application/declaration/DeclarationFixture.java
@@ -0,0 +1,37 @@
+package ang.gimozzi.kickdealbase.application.declaration;
+
+import ang.gimozzi.kickdealbase.domain.declaration.Declaration;
+import ang.gimozzi.kickdealbase.domain.message.Message;
+import ang.gimozzi.kickdealbase.domain.product.Product;
+import ang.gimozzi.kickdealbase.domain.user.User;
+import ang.gimozzi.kickdealbase.infrastructure.persistence.DeclarationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DeclarationFixture {
+
+    private final DeclarationRepository declarationRepository;
+
+    public Declaration reportMessage(User user, Message message, String reason) {
+        return declarationRepository.save(
+                Declaration.messageDeclarationBuilder()
+                        .user(user)
+                        .message(message)
+                        .reason(reason)
+                        .build()
+        );
+    }
+
+    public Declaration reportProduct(User user, Product product, String reason) {
+        return declarationRepository.save(
+                Declaration.productDeclarationBuilder()
+                        .user(user)
+                        .product(product)
+                        .reason(reason)
+                        .build()
+        );
+    }
+
+}
