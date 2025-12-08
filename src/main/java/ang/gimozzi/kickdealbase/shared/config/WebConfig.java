588bdf5a6c4cf0e11c4f3b5d86d93263ffe0c65f
leegunwoo <leegunwoo0325@gmail.com>
2025-12-08 16:03:39 +0900

    i want to work

--- src/main/java/ang/gimozzi/kickdealbase/shared/config/WebConfig.java
+++ src/main/java/ang/gimozzi/kickdealbase/shared/config/WebConfig.java
@@ -1,11 +1,18 @@
 package ang.gimozzi.kickdealbase.shared.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
