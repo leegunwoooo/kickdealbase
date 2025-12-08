4e745b2ba301ba14bf7e1ee657c3d2d0672eb1ea
leegunwoo <leegunwoo0325@gmail.com>
2025-12-09 08:34:46 +0900

    i want to work

--- src/main/java/ang/gimozzi/kickdealbase/application/product/BuyProductUseCase.java
+++ src/main/java/ang/gimozzi/kickdealbase/application/product/BuyProductUseCase.java
@@ -4,6 +4,7 @@ import ang.gimozzi.kickdealbase.domain.product.Product;
 import ang.gimozzi.kickdealbase.domain.product.service.ProductFacade;
 import ang.gimozzi.kickdealbase.domain.user.User;
 
+import ang.gimozzi.kickdealbase.domain.user.service.UserFacade;
 import ang.gimozzi.kickdealbase.infrastructure.s3.S3Service;
 import ang.gimozzi.kickdealbase.presentation.product.dto.ProductResponse;
 import ang.gimozzi.kickdealbase.shared.annotation.UseCase;
@@ -15,14 +16,17 @@ import lombok.RequiredArgsConstructor;
 public class BuyProductUseCase {
 
     private final ProductFacade productFacade;
+    private final UserFacade userFacade;
     private final S3Service s3Service;
 
     @Transactional
     public ProductResponse executeBuy(Long productId, User user) {
         Product product = productFacade.getProduct(productId);
 
-        product.validatePurchaseBy(user);
-        product.purchaseBy(user);
+        User buyer = userFacade.getUser(user.getId());
+
+        product.validatePurchaseBy(buyer);
+        product.purchaseBy(buyer);
 
         String imageUrl = s3Service.generateFileUrl(product.getImageUrl());
         return new ProductResponse(product, imageUrl);
